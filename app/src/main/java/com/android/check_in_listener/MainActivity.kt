c0ea736ee0f2c400ab940b888cbdf1b31577cd48
Suyeon Jang <mmmm07@naver.com>
2021-09-16 21:27:02 +0900

    뷰 바인딩 추가

--- app/src/main/java/com/android/check_in_listener/MainActivity.kt
+++ app/src/main/java/com/android/check_in_listener/MainActivity.kt
@@ -2,10 +2,21 @@ package com.android.check_in_listener
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.android.check_in_listener.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onDestroy() {
+        _binding = null
+        super.onDestroy()
     }
 }
