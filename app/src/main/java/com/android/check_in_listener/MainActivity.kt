1121720229eb6cc2a8d9ef26e6217ed3a48a0c75
YoungSeokHong <80624082+YoungSeokHong@users.noreply.github.com>
2021-09-18 00:49:58 +0900

    리스너(Euphony) 함수 추가 및 MainActivity에 onClickListener 추가

--- app/src/main/java/com/android/check_in_listener/MainActivity.kt
+++ app/src/main/java/com/android/check_in_listener/MainActivity.kt
@@ -2,11 +2,12 @@ package com.android.check_in_listener
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.android.check_in_listener.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() , View.OnClickListener{
 
     private var _binding: ActivityMainBinding? = null
     private val binding get() = _binding!!
@@ -21,10 +22,23 @@ class MainActivity : AppCompatActivity() {
         model.listenData.observe(this, Observer {
             binding.tvNum.text = it.toString()
         })
+
+        binding.btnListen.setOnClickListener(this)
+        binding.btnAllList.setOnClickListener(this)
+
     }
 
     override fun onDestroy() {
         _binding = null
         super.onDestroy()
     }
+
+    override fun onClick(v: View?) {
+        when(v){
+            binding.btnListen ->
+                model.listener()
+        }
+
+    }
+
 }
